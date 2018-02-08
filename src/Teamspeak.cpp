807bc6b05e23d38c18ed88e27144ae1f9933d3ef
Markus Grigull <web@grigull.me>
2018-02-08 21:47:58 +0100

    Add libmicrohttpd submodule dependency

--- src/Teamspeak.cpp
+++ src/Teamspeak.cpp
@@ -33,14 +33,18 @@
 
 #include <iostream>
 #include <stdio.h>
+#include <string.h>
 
-#include "plugin_definitions.h"
+#include <plugin_definitions.h>
+#include <microhttpd.h>
+
 #include "Version.h"
 
 #define PLUGIN_API_VERSION 22;
 
 static struct TS3Functions ts3Functions;
 static char versionBuffer[16];
+static struct MHD_Daemon *httpDaemon;
 
 const char *ts3plugin_name() {
   return "AlternateVoice";
@@ -67,12 +71,35 @@ void ts3plugin_setFunctionPointers(const struct TS3Functions funcs) {
   ts3Functions = funcs;
 }
 
+int answerToConnection(void *cls, struct MHD_Connection *connection, const char *url, const char *method, const char *version, const char *uploadData, size_t *uploadDataSize, void **conCls) {
+  const char *page = "<html><body>Hello World!</body></html>";
+
+  struct MHD_Response *response;
+  int ret;
+
+  response = MHD_create_response_from_buffer(strlen(page), (void *)page, MHD_RESPMEM_PERSISTENT);
+
+  ret = MHD_queue_response(connection, MHD_HTTP_OK, response);
+  MHD_destroy_response(response);
+
+  return ret;
+}
+
 int ts3plugin_init() {
   std::cout << "AlternateVoice: init" << std::endl;
+
+  
+  httpDaemon = MHD_start_daemon(MHD_USE_SELECT_INTERNALLY, 8080, NULL, NULL, &answerToConnection, NULL, MHD_OPTION_END);
+  if (httpDaemon == NULL) {
+    std::cout << "AlternateVoice: Unable to start web server" << std::endl;
+    return 0;
+  }
 
   return 0;
 }
 
 void ts3plugin_shutdown() {
   std::cout << "AlternateVoice: shutdown" << std::endl;
+
+  MHD_stop_daemon(httpDaemon);
 }
